14439833833f59acb766691b0128da9aef35f4d7
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2016-05-23 23:11:23 -0700

    added cli utility

---
+++ bin/bitfield.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+'use strict';
+
+var lib = require('../lib'),
+    fs = require('fs-extra'),
+    yargs = require('yargs'),
+    onml = require('onml');
+
+var argv = yargs.count('icestorm').argv;
+var fileName;
+
+if (argv._.length === 1) {
+    fileName = argv._[0];
+    fs.readJson(fileName, function (err, src) {
+        var res = lib.render(src);
+        var svg = onml.s(res);
+        console.log(svg);
+    });
+} else {
+    console.log(argv);
+}
