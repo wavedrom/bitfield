0508b27f58cd5818373acab32903ac3a99c1e149
K4zuki <k.yamamoto.08136891@gmail.com>
2017-01-30 01:47:52 +0900

    NOT for merge: reuse default option values if not specified
    - no more need to set all named options
    - add "font_family" named option to set font family
      - multiple "style" option is not ready

--- bin/bitfield.js
+++ bin/bitfield.js
@@ -16,14 +16,21 @@ var options = {
 };
 
 if (argv.input){
-    if (argv.vspace && argv.hspace && argv.lanes && argv.bits){
-        options = {
-            vspace: argv.vspace,
-            hspace: argv.hspace,
-            lanes: argv.lanes,
-            bits: argv.bits
-        };
-    }
+    if(argv.vspace){options.vspace = argv.vspace}
+    if(argv.hspace){options.hspace = argv.hspace}
+    if(argv.lanes){options.lanes = argv.lanes}
+    if(argv.bits){options.bits = argv.bits}
+    if(argv.font_family){options.font_family = argv.font_family}
+
+    // if (argv.vspace && argv.hspace && argv.lanes && argv.bits){
+    //   // console.log(options.vspace);
+    //     options = {
+    //         vspace: argv.vspace,
+    //         hspace: argv.hspace,
+    //         lanes: argv.lanes,
+    //         bits: argv.bits
+    //     };
+    // }
     fileName = argv.input;
     fs.readJson(fileName, function (err, src) {
         var res = lib.render(src, options);
