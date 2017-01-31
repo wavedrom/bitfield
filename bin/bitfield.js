9b63f15ec8c06073e42bea4f4181d4c5342056c8
Kazuki Yamamoto <k.yamamoto.08136891@gmail.com>
2017-01-31 22:41:35 +0900

    preset font default value - otherwise blank rectangle generated

--- bin/bitfield.js
+++ bin/bitfield.js
@@ -20,9 +20,21 @@ if (argv.input){
     if(argv.hspace){options.hspace = argv.hspace}
     if(argv.lanes){options.lanes = argv.lanes}
     if(argv.bits){options.bits = argv.bits}
-    if(argv.fontfamily){options.fontfamily = argv.fontfamily}
-    if(argv.fontweight){options.fontweight = argv.fontweight}
-    if(argv.fontsize){options.fontsize = argv.fontsize}
+    if(argv.fontfamily){
+      options.fontfamily = argv.fontfamily
+    }else{
+      options.fontfamily = "sans-serif" // default
+    }
+    if(argv.fontweight){
+      options.fontweight = argv.fontweight
+    }else{
+      options.fontweight = "normal" // default
+    }
+    if(argv.fontsize){
+      options.fontsize = argv.fontsize
+    }else{
+      options.fontsize = 14 // default
+    }
 
     fileName = argv.input;
     fs.readJson(fileName, function (err, src) {
