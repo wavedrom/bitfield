62e87e7d9019493c3d74fc5d420dac5873637507
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2017-11-07 15:55:37 -0800

    moved defaults into the library code

--- bin/bitfield.js
+++ bin/bitfield.js
@@ -9,21 +9,10 @@ var lib = require('../lib'),
 var argv = yargs.argv;
 var fileName;
 
-var options = {
-    vspace: argv.vspace || 80,
-    hspace: argv.hspace || 640,
-    lanes:  argv.lanes  || 2,
-    bits:   argv.bits   || 32,
-    bigendian: argv.gigendian || false,
-    fontfamily: argv.fontfamily || 'sans-serif',
-    fontweight : argv.fontweight || 'normal',
-    fontsize : argv.fontsize || 14,
-};
-
 if (argv.input) {
     fileName = argv.input;
     fs.readJson(fileName, function (err, src) {
-        var res = lib.render(src, options);
+        var res = lib.render(src, argv);
         var svg = onml.s(res);
         console.log(svg);
     });
