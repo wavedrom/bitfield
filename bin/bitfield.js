3b8f664c4387276a1d0c84771d12cd24930e6ff6
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2019-02-16 11:56:03 -0800

    improved CLI

--- bin/bitfield.js
+++ bin/bitfield.js
@@ -1,23 +1,28 @@
 #!/usr/bin/env node
 'use strict';
 
-var lib = require('../lib'),
-    fs = require('fs-extra'),
-    yargs = require('yargs'),
-    onml = require('onml');
+var fs = require('fs-extra');
+var yargs = require('yargs');
+var onml = require('onml');
 
-var argv = yargs.argv;
+var lib = require('../lib');
+
+var argv = yargs
+    .option('input', {
+        alias: 'i',
+        describe: 'path to the source'
+    })
+    .demandOption(['input'])
+    .help()
+    .argv;
+
 var fileName;
 
-if (argv.input) {
-    fileName = argv.input;
-    fs.readJson(fileName, function (err, src) {
-        var res = lib.render(src, argv);
-        var svg = onml.s(res);
-        console.log(svg);
-    });
-} else {
-    console.log(argv);
-}
+fileName = argv.input;
+fs.readJson(fileName, function (err, src) {
+    var res = lib.render(src, argv);
+    var svg = onml.s(res);
+    console.log(svg);
+});
 
 /* eslint no-console: 0 */
