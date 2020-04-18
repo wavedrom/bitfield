8cf2d35295ffba7128805ecb2d1295784e0e8055
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2020-04-17 17:49:35 -0700

    2020 js modernization

--- bin/bitfield.js
+++ bin/bitfield.js
@@ -8,27 +8,27 @@ var onml = require('onml');
 var lib = require('../lib');
 
 var argv = yargs
-    .option('input', {describe: 'path to the source', alias: 'i'})
-    .option('vspace', {describe: 'vertical space', default: 80})
-    .option('hspace', {describe: 'horizontal space', default: 640})
-    .option('lanes', {describe: 'rectangle lanes', default: 2})
-    .option('bits', {describe: 'overall bitwidth', default: 32})
-    .option('fontsize', {describe: 'font size', default: 14})
-    .option('bigendian', {describe: 'endianness', default: false})
-    .option('compact', {describe: 'compact format', default: false})
-    .option('fontfamily', {describe: 'font family', default: 'sans-serif'})
-    .option('fontweight', {describe: 'font weight', default: 'normal'})
-    .demandOption(['input'])
-    .help()
-    .argv;
+  .option('input', {describe: 'path to the source', alias: 'i'})
+  .option('vspace', {describe: 'vertical space', default: 80})
+  .option('hspace', {describe: 'horizontal space', default: 640})
+  .option('lanes', {describe: 'rectangle lanes', default: 2})
+  .option('bits', {describe: 'overall bitwidth', default: 32})
+  .option('fontsize', {describe: 'font size', default: 14})
+  .option('bigendian', {describe: 'endianness', default: false})
+  .option('compact', {describe: 'compact format', default: false})
+  .option('fontfamily', {describe: 'font family', default: 'sans-serif'})
+  .option('fontweight', {describe: 'font weight', default: 'normal'})
+  .demandOption(['input'])
+  .help()
+  .argv;
 
 var fileName;
 
 fileName = argv.input;
 fs.readJson(fileName, function (err, src) {
-    var res = lib.render(src, argv);
-    var svg = onml.s(res);
-    console.log(svg);
+  var res = lib.render(src, argv);
+  var svg = onml.s(res, 2);
+  console.log(svg);
 });
 
 /* eslint no-console: 0 */
--- test/basic.js
+++ test/basic.js
@@ -1,42 +1,49 @@
 'use strict';
 
-var lib = require('../lib'),
-    onml = require('onml'),
-    jsof = require('jsof'),
-    alpha = require('./alpha.json');
-    // expect = require('chai').expect;
+const lib = require('../lib');
+const onml = require('onml');
+const jsof = require('jsof');
+const alpha = require('./alpha.json');
+// expect = require('chai').expect;
 
-var dat = {
-    word: {
-        src: [{ name: 'data', bits: 32 }],
-        dst: []
-    },
-    bytes: {
-        src: alpha,
-        dst: [],
-        opt: { hspace: 800 }
+const dat = {
+  word: {
+    src: [{
+      name: 'data',
+      bits: 32
+    }],
+    dst: []
+  },
+  bytes: {
+    src: alpha,
+    dst: [],
+    opt: {
+      hspace: 800
     }
+  }
 };
-
-describe('basic', function () {
-    Object.keys(dat).forEach(function (key) {
-        it(key, function (done) {
-            var src = dat[key].src;
-            var dst = dat[key].dst;
-            var opt = dat[key].opt;
-            var res = lib.render(src, opt);
-            try {
-                // expect(res).to.deep.eq(dst);
-                console.log(onml.stringify(res));
-            } catch (err) {
-                console.log(jsof.s(res));
-                throw err;
-            }
-            dst.unshift('text', {x: 20, y: 20, 'font-size': 16 });
-            // var svg = ['svg', { viewBox: '0 0 400 100', width: 400, height: 100, xmlns: 'http://www.w3.org/2000/svg' }, dst];
-            done();
-        });
+describe('basic', function() {
+  Object.keys(dat).forEach(function(key) {
+    it(key, function(done) {
+      var src = dat[key].src;
+      var dst = dat[key].dst;
+      var opt = dat[key].opt;
+      var res = lib.render(src, opt);
+      try {
+        // expect(res).to.deep.eq(dst);
+        console.log(onml.stringify(res, 2));
+      } catch (err) {
+        console.log(jsof.s(res));
+        throw err;
+      }
+      dst.unshift('text', {
+        x: 20,
+        y: 20,
+        'font-size': 16
+      });
+      // var svg = ['svg', { viewBox: '0 0 400 100', width: 400, height: 100, xmlns: 'http://www.w3.org/2000/svg' }, dst];
+      done();
     });
-});
-/* eslint no-console: 0 */
+  });
+});/* eslint no-console: 0 */
 /* eslint-env mocha */
